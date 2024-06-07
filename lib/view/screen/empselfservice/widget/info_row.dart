f39e8c5a725c516bd2faca9b9a4c11635ab79638
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-06-07 21:39:52 +0600

    designed leave approval page with dummy data

---
+++ lib/view/screen/empselfservice/widget/info_row.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+
+class InfoRow extends StatelessWidget {
+  final String label;
+  final String value;
+
+  InfoRow({required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Text(
+              label,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text(
+              value,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
