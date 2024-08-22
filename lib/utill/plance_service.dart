f19081e5f3dd507110c57046ca7d707fe624398f
Md. Rafiqul Islam <rafiq@Mds-MacBook-Air.local>
2024-08-22 11:45:23 +0600

    pod install for ios

--- lib/utill/plance_service.dart
+++ lib/utill/plance_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+//import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
