e8da61ee69b9ed7f1bfb40938e8f3157cac7005c
Habibur Rahman <habiburkuetacbd@gmail.com>
2025-08-24 15:10:00 +0600

    rseolved conficted

--- lib/utill/app_constants.dart
+++ lib/utill/app_constants.dart
@@ -4,10 +4,10 @@ import 'package:geolocator/geolocator.dart';
 class AppConstants {
   static const String APP_NAME = 'SMART';
 
-  // static const String BASE_URL = 'https://smartapp.ssgil.com/'; // live
+  static const String BASE_URL = 'https://smartapp.ssgil.com/'; // live
   //  static const String BASE_URL = 'http://10.25.4.120/'; //production
 
-  static const String BASE_URL = 'http://10.27.1.177/api/'; //oracle cloud test
+  // static const String BASE_URL = 'http://10.27.1.177/api/'; //oracle cloud test
   // static const String BASE_URL = 'http://10.0.2.2/'; //android local ip
   // static const String BASE_URL = 'http://192.168.1.102/'; //HOME IP
   // static const String BASE_URL = 'http://172.29.10.250/api/'; // Ofiice IP
@@ -18,6 +18,7 @@ class AppConstants {
   static const double GEO_FENCING_DISTANCE = 100; // in meter
   static const LocationAccuracy Location_Accuracy = LocationAccuracy.best;
   static const int BATTARY_SERIAL_NO_SIZE = 10;
+
 
   // API Methods
   static const String LOGIN_URI = 'login';
